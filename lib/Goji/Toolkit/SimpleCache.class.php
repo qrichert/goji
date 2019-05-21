42b5725ac59d2fc52dde2eabe2de967ad4eaa7d3
Quentin Richert <quentin@quentinrichert.com>
2019-05-21 19:39:57 +0200

    [Caching] Add caching policy options

--- lib/Goji/Toolkit/SimpleCache.class.php
+++ lib/Goji/Toolkit/SimpleCache.class.php
@@ -2,8 +2,11 @@
 
 	namespace Goji\Toolkit;
 
+	use Exception;
+
 	// If SimpleCache is called from a __destruct(), relative path may vary
 	// Here we make sure it stays the same
+	// TODO: bro, set it in config
 	if (!defined('SIMPLE_CACHE_PATH')) {
 
 		$cachePath = '../var/cache/';
@@ -25,7 +28,12 @@
 	 *
 	 * A class to use caching in a very simple way.
 	 *
-	 * This class is made to be used statically. Following are some examples of use.
+	 * This class is made to be used statically.
+	 *
+	 * For more information about HTTP caching, see:
+	 * https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching
+	 *
+	 * Following are some examples of use.
 	 *
 	 * Caching a simple fragment:
 	 * --------------------------
@@ -87,6 +95,8 @@
 	 */
 	class SimpleCache {
 
+		/* <CONSTANTS> */
+
 		const CACHE_PATH = SIMPLE_CACHE_PATH;
 		const CACHE_FILE_EXTENSION = '.cache.txt';
 		// TODO: Make DEFAULT_CACHE_MAX_AGE settable
@@ -101,6 +111,15 @@
 		const TIME_1WEEK = 7 * self::TIME_1DAY;
 		const TIME_1MONTH = 30 * self::TIME_1DAY;
 		const TIME_1YEAR = 365 * self::TIME_1DAY;
+
+		const HTTP_CACHE_CONTROL_NO_CACHE = 'no-cache';
+		const HTTP_CACHE_CONTROL_NO_STORE = 'no-store';
+		const HTTP_CACHE_CONTROL_PUBLIC = 'public';
+		const HTTP_CACHE_CONTROL_PRIVATE = 'private';
+
+		const E_WRONG_HTTP_CACHE_RESTRICTION_SETTING = 0;
+		const E_WRONG_HTTP_CACHE_PRIVACY_SETTING = 1;
+		const E_HTTP_CACHE_MAX_AGE_INVALID = 3;
 
 /* <GENERIC BUFFERING FUNCTIONS> */
 
@@ -539,4 +558,143 @@
 
 			return $cacheId;
 		}
+
+/* <HTTP CACHING HEADERS> */
+
+		/**
+		 * HTTP Entity Tag (ETag) token
+		 *
+		 * ETag is sort of a state token. It's an unique ID of a file and its content (like a hash).
+		 * It is used by to browser to check whether it should update the file in cache or not.
+		 *
+		 * For example a file hello.txt containing 'hello'. We could hash it and obtain a token
+		 * like a5rX98j. So as long as the token remains the same, the browser know it doesn't
+		 * need to update the file since it hasn't changed.
+		 *
+		 * But let's change the content of the file to 'hello, world!'. The new hash is klg83Ds.
+		 * The browser notices it has changed and thus request the updated version.
+		 *
+		 * ETag is checked once (and only once) the cache has expired. Once the file in cache has expired
+		 * (max-age) the browsers requests the ETag. If it's the same, the file is cached again for max-age
+		 * (part of the HTTP response) without re-download. It it doesn't match, the new version is downloaded.
+		 *
+		 * No data is transferred (apart from the header) if ETag is valid.
+		 *
+		 * @param string $token
+		 */
+		private static function setHttpETag(string $token): void {
+			header("ETag: $token");
+		}
+
+		/**
+		 * HTTP Cache Control Restriction (no-cache | no-store)
+		 *
+		 * no-cache: With the no-cache header, the browser CAN cache the file but MUST re-validate it for
+		 * each request. It's like max-age was set to 0. If the file is still the same (see ETag) it uses
+		 * the cached version, else it re-downloads it.
+		 *
+		 * no-store: With no-store, the cache is completely disabled for the given file. This is used for
+		 * privacy or if the file always changes. It's best not to store banking information and so on
+		 * inside the browser cache.
+		 *
+		 * @param string $directive (HTTP_CACHE_CONTROL_NO_STORE | HTTP_CACHE_CONTROL_NO_CACHE)
+		 */
+		private static function setHttpCacheControlRestriction(string $directive): void {
+			header("Cache-Control: $directive", false);
+		}
+
+		/**
+		 * HTTP Cache Control Privacy (public | private)
+		 *
+		 * public: public indicates the file can be cache in any case, regardess of other settings
+		 * like HTTP authentication or non-cachable status codes. You don't need it in most cases
+		 * where you'd use max-age, because max-age implies public.
+		 *
+		 * private: private means the end user (browser) is allowed to cache the file, but not any intermediate
+		 * cache system (like a CDN). This is meant for privacy also.
+		 *
+		 * @param string $directive (HTTP_CACHE_CONTROL_PUBLIC | HTTP_CACHE_CONTROL_PRIVATE)
+		 */
+		private static function setHttpCacheControlPrivacy(string $directive): void {
+			header("Cache-Control: $directive", false);
+		}
+
+		/**
+		 * HTTP Cache Control Max Age (in seconds)
+		 *
+		 * How long (in seconds) the browser should consider the cached file valid until it must re-validate.
+		 * Be careful, no re-validation will occur until the 'countdown' is over (or the user empties his cache).
+		 *
+		 * If you set a CSS file for 1 year for example, and the user doesn't empty his cache, he will keep
+		 * the same design CSS file for 1 year, regardless of how many updates you make.
+		 *
+		 * A way around it is changing the name of the file.
+		 *
+		 * /!\ If you don't use any cache directive, it's up to the browser to decide what to do /!\
+		 *
+		 * @param int $maxAge In seconds
+		 */
+		private static function setHttpCacheMaxAge(int $maxAge): void {
+			header("Cache-Control: max-age=$maxAge", false);
+		}
+
+		public static function setHttpCachingPolicy(array $policy): void {
+
+			$eTag = $policy['etag'] ?? null;
+			$restriction = $policy['restriction'] ?? null;
+			$privacy = $policy['privacy'] ?? null;
+			$maxAge = $policy['max-age'];
+
+			if (isset($eTag)) {
+				self::setHttpETag($eTag);
+			}
+
+			if (isset($restriction)) {
+
+				if ($restriction === self::HTTP_CACHE_CONTROL_NO_CACHE
+				    || $restriction === self::HTTP_CACHE_CONTROL_NO_STORE)
+					self::setHttpCacheControlRestriction($restriction);
+				else
+					throw new Exception("HTTP Cache Control Restriction not valid: $restriction. Must be either no-cache or no-store.", self::E_WRONG_HTTP_CACHE_RESTRICTION_SETTING);
+			}
+
+			if (isset($privacy)) {
+
+				if ($privacy === self::HTTP_CACHE_CONTROL_PUBLIC
+				    || $privacy === self::HTTP_CACHE_CONTROL_PRIVATE)
+					self::setHttpCacheControlPrivacy($privacy);
+				else
+					throw new Exception("HTTP Cache Control Privacy not valid: $privacy. Must be either public or private.", self::E_WRONG_HTTP_CACHE_PRIVACY_SETTING);
+			}
+
+			if (isset($maxAge)) {
+
+				if (is_numeric($maxAge))
+					// To positive int
+					self::setHttpCacheMaxAge(abs(intval($maxAge)));
+				else
+					throw new Exception('Http Cache Max Age invalid. Must be a numeric value in seconds.');
+
+			}
+		}
+
+		/*private static function httpCacheMustRevalidate(){
+			isset(If-None-Match) ?
+			token = If-None-Match
+
+			if (token === currentToken)
+			 http header: 304 not modified
+			 return false
+
+			else
+			 http set Etag: new token
+			 http max-age = XXX
+			 return true
+		}*/
+
+		/*private static function httpIfModifiedSince() {
+			Similar to ETag but with timestamp
+			If-Modified-Since: Fri, 13 Jul 2018 10:49:23 GMT
+				http set last-modified: Fri, 10 Jul 2018 10:49:23 GMT /
+		}*/
 	}
