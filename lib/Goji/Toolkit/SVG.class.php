57632403ee21ed23969864b2794022306f2185af
Quentin Richert <quentin@quentinrichert.com>
2019-06-24 17:43:04 +0200

    [Toolkit] Added SVG file includer

---
+++ lib/Goji/Toolkit/SVG.class.php
@@ -0,0 +1,46 @@
+<?php
+
+	namespace Goji\Toolkit;
+
+	use Exception;
+	use Goji\Core\Logger;
+
+	/**
+	 * Class SVG
+	 *
+	 * @package Goji\Toolkit
+	 */
+	class SVG {
+
+		/* <CONSTANTS> */
+
+		const E_FILE_CANNOT_BE_READ = 0;
+
+		/**
+		 * Outputs SVG code starting at <svg... (without XML declaration)
+		 *
+		 * @param $file
+		 */
+		public static function includeFile($file): void {
+
+			$f = fopen($file, 'r');
+
+			if ($f === false)
+				return;
+
+			while ($line = fgets($f)) {
+
+				$svgTagPos = mb_stripos($line, '<svg '); // mb_stripos() == mb_strpos() but case insensitive
+
+				if ($svgTagPos !== false) {
+
+					echo mb_substr($line, $svgTagPos);
+					fpassthru($f);
+
+					break;
+				}
+			}
+
+			fclose($f);
+		}
+	}
