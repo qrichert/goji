c5f64b483d172443e9dbbb45b88a139e240b0bb7
Quentin Richert <quentin@quentinrichert.com>
2019-09-14 16:35:36 +0200

    [Appointment] Set-up appointment page

---
+++ lib/Goji/Appointments/Scheduler.class.php
@@ -0,0 +1,22 @@
+<?php
+
+	namespace Goji\Appointments;
+
+	/**
+	 * Class Scheduler
+	 *
+	 * @package Goji\Appointments
+	 */
+	class Scheduler {
+
+		/* <ATTRIBUTES> */
+
+		/* <CONSTANTS> */
+
+		public function __construct() {
+		}
+
+		public function render(): void {
+			echo '__CALENDAR__';
+		}
+	}
--- src/View/AppointmentView.php
+++ src/View/AppointmentView.php
@@ -1,5 +1,8 @@
 <main>
 	<section class="text">
 		<h1><?= $tr->_('APPOINTMENT_MAIN_TITLE'); ?></h1>
+		<p>
+			<?= $tr->_('APPOINTMENT_HELP_TEXT'); ?>
+		</p>
 	</section>
 </main>
