409e73d997149378d7744e417b4eb96fd8dd9cd0
Quentin Richert <quentin@quentinrichert.com>
2019-10-21 11:53:06 +0200

    Moved Login/Logout controllers to Controller\HR namespace

--- src/Controller/HR/LoginController.class.php
+++ src/Controller/HR/LoginController.class.php
@@ -1,6 +1,6 @@
 <?php
 
-	namespace App\Controller;
+	namespace App\Controller\HR;
 
 	use App\Model\LoginForm;
 	use Goji\Blueprints\HttpMethodInterface;
--- src/Controller/HR/LogoutController.class.php
+++ src/Controller/HR/LogoutController.class.php
@@ -1,6 +1,6 @@
 <?php
 
-	namespace App\Controller;
+	namespace App\Controller\HR;
 
 	use Goji\Blueprints\ControllerAbstract;
 	use Goji\Core\HttpResponse;
