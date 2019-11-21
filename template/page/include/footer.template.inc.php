1f61e2ffb8ff746529769efd3efc55869bb66b24
Quentin Richert <quentin@quentinrichert.com>
2019-11-21 10:19:41 +0100

    Added 'Admin' link to footer

--- template/page/include/footer.template.inc.php
+++ template/page/include/footer.template.inc.php
@@ -2,36 +2,36 @@
 	<div class="footer__container">
 		<!-- Left -->
 		<div class="footer__main-content">
-			<!-- Children aligned in row -->
+			<!-- Children aligned in row (flex: row) -->
 			<!--<p>© <?= date('Y'); ?> Goji, Inc.</p>-->
 			<div>
-				<!-- Sub-children aligned in column -->
-				<p>
-					<a href="https://github.com/qrichert/goji" target="_blank">
-						<?= $tr->_('FOOTER_GOJI_ON_GITHUB'); ?>
-					</a>
-				</p>
-				<p>
-					<a href="<?= $this->m_app->getRouter()->getLinkForPage('privacy-and-terms'); ?>" rel="nofollow">
-						<?= $tr->_('FOOTER_PRIVACY_AND_TERMS'); ?>
-					</a>
-				</p>
+				<!-- Sub-children aligned in column if parent is a <div> (flex: column) -->
+				<a href="https://github.com/qrichert/goji" target="_blank">
+					<?= $tr->_('FOOTER_GOJI_ON_GITHUB'); ?>
+				</a>
+
+				<a href="<?= $this->m_app->getRouter()->getLinkForPage('privacy-and-terms'); ?>" rel="nofollow">
+					<?= $tr->_('FOOTER_PRIVACY_AND_TERMS'); ?>
+				</a>
 			</div>
-			<p>
-				<?php
-					if ($this->m_app->getUser()->isLoggedIn()) {
-					?>
-						<a href="<?= $this->m_app->getRouter()->getLinkForPage('logout'); ?>" rel="nofollow"><?= $tr->_('FOOTER_LOG_OUT'); ?></a>
-					<?php
-					} else {
-					?>
-						<a href="<?= $this->m_app->getRouter()->getLinkForPage('login'); ?>" rel="nofollow"><?= $tr->_('FOOTER_LOG_IN'); ?></a>
-					<?php
-					}
+			<?php
+				if ($this->m_app->getUser()->isLoggedIn()) {
 				?>
-			</p>
-			<!--<p><a href="#">Terms</a></p>-->
-			<!--<p><a href="#">Privacy</a></p>-->
+					<a href="<?= $this->m_app->getRouter()->getLinkForPage('logout'); ?>" rel="nofollow"><?= $tr->_('FOOTER_LOG_OUT'); ?></a>
+				<?php
+				} else {
+				?>
+					<a href="<?= $this->m_app->getRouter()->getLinkForPage('login'); ?>" rel="nofollow"><?= $tr->_('FOOTER_LOG_IN'); ?></a>
+				<?php
+				}
+
+				if ($this->m_app->getUser()->isLoggedIn()
+				    && $this->m_app->getMemberManager()->memberIs('editor')) {
+				?>
+					<a href="<?= $this->m_app->getRouter()->getLinkForPage('admin'); ?>" rel="nofollow"><?= $tr->_('FOOTER_ADMIN'); ?></a>
+				<?php
+				}
+			?>
 			<p>Made with ❤️ and PHP</p>
 		</div>
 		<!-- Right -->
