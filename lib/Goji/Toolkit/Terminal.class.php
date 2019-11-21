1fbcb5fda3bb996461df278b19aad187d205e21f
Quentin Richert <quentin@quentinrichert.com>
2019-11-21 13:13:45 +0100

    Added return status (command succes or fail)

--- lib/Goji/Toolkit/Terminal.class.php
+++ lib/Goji/Toolkit/Terminal.class.php
@@ -11,7 +11,7 @@
 		const SYSTEM = 'system';
 		const PASSTHRU = 'passthru';
 		const EXEC = 'exec';
-		const SHELL_EXEC = 'shell_exec';
+//		const SHELL_EXEC = 'shell_exec';
 
 		const E_COMMAND_DOES_NOT_EXIST = 0;
 		const E_COMMAND_EXECUTION_NOT_POSSIBLE = 0;
@@ -27,11 +27,12 @@
 		 * given command and outputs the result.
 		 *
 		 * @param string $command
+		 * @param int $return_var
 		 * @return string
 		 */
-		private static function system(string $command): string {
+		private static function system(string $command, int &$return_var = null): string {
 			ob_start();
-			system($command);
+			system($command, $return_var);
 			return ob_get_clean();
 		}
 
@@ -46,11 +47,12 @@
 		 * to the browser.
 		 *
 		 * @param string $command
+		 * @param int $return_var
 		 * @return string
 		 */
-		private static function passthru(string $command): string {
+		private static function passthru(string $command, int &$return_var = null): string {
 			ob_start();
-			passthru($command);
+			passthru($command, $return_var);
 			return ob_get_clean();
 		}
 
@@ -62,10 +64,11 @@
 		 * exec() executes the given command.
 		 *
 		 * @param string $command
+		 * @param int $return_var
 		 * @return string
 		 */
-		private static function exec(string $command): string {
-			exec($command , $output);
+		private static function exec(string $command, int &$return_var = null): string {
+			exec($command , $output, $return_var);
 			return implode("\n" , $output) . "\n";
 		}
 
@@ -85,52 +88,69 @@
 
 		/**
 		 * @param string $command
+		 * @param bool $success
 		 * @param null $forceCommand
 		 * @return string
 		 * @throws \Exception
 		 */
-		protected static function processCommand(string $command, $forceCommand = null): string {
+		protected static function processCommand(string $command, bool &$success = null, $forceCommand = null): string {
 
 			// Redirect stderr to stdout
 			$command .= ' 2>&1';
 
+			$output = '';
+			$return_var = 0;
+
 			if ($forceCommand !== null) {
 
 				switch ($forceCommand) {
-					case self::SYSTEM:      return self::system($command);      break;
-					case self::PASSTHRU:    return self::passthru($command);    break;
-					case self::EXEC:        return self::exec($command);        break;
-					case self::SHELL_EXEC:  return self::shell_exec($command);  break;
+					case self::SYSTEM:      $output = self::system($command, $return_var);      break;
+					case self::PASSTHRU:    $output = self::passthru($command, $return_var);    break;
+					case self::EXEC:        $output = self::exec($command, $return_var);        break;
+//					case self::SHELL_EXEC:  $output = self::shell_exec($command);  break;
 					default:
 						throw new Exception("Command '$forceCommand' doesn't exist", self::E_COMMAND_DOES_NOT_EXIST);
 						break;
 				}
+
+			} else {
+
+				if (function_exists('system'))
+					$output = self::system($command, $return_var);
+				else if (function_exists('passthru'))
+					$output = self::passthru($command, $return_var);
+				else if (function_exists('exec'))
+					$output = self::exec($command, $return_var);
+//  			else if (function_exists('shell_exec'))
+//	    			$output = self::shell_exec($command);
+				else
+					throw new Exception('Command execution not possible on this system.', self::E_COMMAND_EXECUTION_NOT_POSSIBLE);
 			}
 
-			if (function_exists('system'))
-				return self::system($command);
-			else if (function_exists('passthru'))
-				return self::passthru($command);
-			else if (function_exists('exec'))
-				return self::exec($command);
-			else if (function_exists('shell_exec'))
-				return self::shell_exec($command);
-			else
-				throw new Exception('Command execution not possible on this system.', self::E_COMMAND_EXECUTION_NOT_POSSIBLE);
+			$success = $return_var === 0; // 0 = no error
+
+			return $output;
 		}
 
 		/**
-		 * @param string $command
-		 * @return string
+		 * @param string $command Command(s) (can me multiple with && or ;)
+		 * @param bool|null $success 'returns' true if no error, false if error
+		 * @param string|null $forceCommand Terminal::SYSTEM|PASSTHRU|EXEC
+		 * @return string Command(s) output
 		 * @throws \Exception
 		 */
-		public static function execute(string $command, string $forceCommand = null): string {
+		public static function execute(string $command, bool &$success = null, string $forceCommand = null): string {
 
 			$output = '';
 			$command = preg_split('#(&&|;)#', $command);
+			$success = true; // True unless error
 
 			foreach ($command as $c) {
-				$output .= self::processCommand($c, $forceCommand);
+
+				$output .= self::processCommand($c, $commandSuccess, $forceCommand);
+
+				if ($commandSuccess === false)
+					$success = false;
 			}
 
 			return $output;
