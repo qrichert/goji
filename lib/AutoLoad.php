f26677d89a815fc88622b414a5e42714f83072ad
Quentin Richert <quentin@quentinrichert.com>
2019-12-10 12:07:37 +0100

    Transferred autoloading functions to \Goji\Core\AutoLoader

--- lib/AutoLoad.php
+++ lib/AutoLoad.php
@@ -2,52 +2,7 @@
 
 	namespace AutoLoad;
 
-	/**
-	 * Auto-load function for libraries.
-	 *
-	 * Libraries must be in their own project folder inside /lib, and their
-	 * namespace must reflect that architecture.
-	 *
-	 * For example, SimpleCache class from Goji Toolkit library is in
-	 * Goji\Toolkit\SimpleCache namespace. So, auto-loading it will look for
-	 * SimpleCache.class.php inside a Goji/Toolkit folder, inside /lib.
-	 *
-	 * \Goji\Toolkit\SimpleCache -> '/lib/' + 'Goji/Toolkit/SimpleCache' + '.class.php'
-	 *
-	 * @param string $className The class which needs to be loaded (no initial backslash '\')
-	 */
-	function autoLoadLibrary($className) {
+	require_once 'Goji/Core/AutoLoader.class.php';
 
-		// There shouldn't be a leading backslash, but you never know
-		$className = ltrim($className, '\\');
-
-		// Goji\SimpleTemplate -> ../lib/Goji/SimpleTemplate.class.php
-		$classFile = ROOT_PATH . '/lib/' . str_replace('\\', '/', $className) . '.class.php';
-
-		if (is_file($classFile))
-			require_once $classFile;
-	}
-
-	/**
-	 * Auto-load function for source files.
-	 *
-	 * Source files must be in /src folder.
-	 *
-	 * \App\Model\HomeModel -> '/src/' + 'Model/HomeModel' + '.class.php'
-	 *
-	 * @param string $className The class which needs to be loaded (no initial backslash '\')
-	 */
-	function autoLoadSource($className) {
-
-		// There shouldn't be a leading backslash, but you never know
-		$className = ltrim($className, '\\');
-
-		// App/Model/HomeController -> ../src/App/Model/HomeModel.class.php
-		$classFile = ROOT_PATH . '/src/' . str_replace('\\', '/', $className) . '.class.php';
-
-		if (is_file($classFile))
-			require_once $classFile;
-	}
-
-	spl_autoload_register('AutoLoad\autoLoadLibrary');
-	spl_autoload_register('AutoLoad\autoLoadSource');
+	spl_autoload_register('\Goji\Core\AutoLoader::autoLoadLibrary');
+	spl_autoload_register('\Goji\Core\AutoLoader::autoLoadSource');
