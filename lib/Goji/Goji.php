52003c642c6c50378ca179c509cafc3ca6ff72b1
Quentin Richert <quentin@quentinrichert.com>
2020-01-04 10:58:02 +0100

    Simplified includes to one file

---
+++ lib/Goji/Goji.php
@@ -0,0 +1,5 @@
+<?php
+
+	require_once '../lib/Settings.php';
+	require_once '../lib/RootPath.php';
+	require_once '../lib/AutoLoad.php';
--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 
-	require_once '../lib/Settings.php';
-	require_once '../lib/RootPath.php';
-	require_once '../lib/AutoLoad.php';
+	require_once '../lib/Goji/Goji.php';
 
 	use Goji\Core\App;
 
