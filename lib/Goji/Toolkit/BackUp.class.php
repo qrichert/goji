82c049e1bc1cbd223935306829b7db664f712dff
Quentin Richert <quentin@quentinrichert.com>
2020-02-11 17:02:54 +0100

    Remove old database backups on backup

--- lib/Goji/Toolkit/BackUp.class.php
+++ lib/Goji/Toolkit/BackUp.class.php
@@ -18,6 +18,14 @@ class BackUp {
 
 	const DATABASE_PREFIX = 'db__';
 
+	/**
+	 * Backup SQLite database file
+	 *
+	 * @param \Goji\Core\Database $db
+	 * @param bool $addBackupDate
+	 * @param bool $addFileMTime
+	 * @return bool
+	 */
 	public static function database(Database $db, bool $addBackupDate = true, bool $addFileMTime = true): bool {
 
 		$dbFile = $db->getDatabaseFile();
@@ -38,6 +46,42 @@ class BackUp {
 		if (!is_dir(self::BACKUP_PATH))
 			mkdir(self::BACKUP_PATH, 0777, true);
 
-		return copy($dbFile, $backupFile);
+		if (!copy($dbFile, $backupFile))
+			return false;
+
+		// Remove old backups
+		self::removeOldBackupFiles(self::DATABASE_PREFIX);
+
+		return true;
+	}
+
+	/**
+	 * Removes oldest files (sorted by name) if number of files exceeds $maxCount
+	 *
+	 * @param string $prefix What prefix is concerned (ex: db__*)
+	 * @param int $maxCount How many should be kept (optional, default = 15)
+	 * @return bool
+	 */
+	public static function removeOldBackupFiles(string $prefix, int $maxCount = 15): bool {
+
+		$pattern = self::BACKUP_PATH . $prefix . '*' . self::BACKUP_FILE_EXTENSION;
+
+		$backupFiles = glob($pattern);
+		// Get them sorted in natural order, newest first
+		rsort($backupFiles, SORT_NATURAL);
+
+		$count = count($backupFiles);
+
+		$success = true;
+
+		for ($i = 0; $i < $count; $i++) {
+			// Remove file if we are over $maxCount
+			if ($i >= $maxCount) {
+				if (!unlink($backupFiles[$i]))
+					$success = false;
+			}
+		}
+
+		return $success;
 	}
 }
