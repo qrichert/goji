c04d1a714febfa7f425e2ca0a1213e083f4dd201
Quentin Richert <quentin@quentinrichert.com>
2020-05-22 14:31:02 +0200

    Added helper for adding terminal symlink

---
+++ bin/add-terminal.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: text/plain');
+
+$return_var = null;
+
+ob_start();
+system('ln -sfv ../../_terminal/public/ ./terminal', $return_var);
+$output = ob_get_clean();
+
+if ($return_var === 0) // 0 = no error
+	echo "OK";
+else
+	echo "ERROR: $return_var";
+
+echo PHP_EOL;
+echo getcwd();
+echo PHP_EOL;
+echo $output;
+echo PHP_EOL;
+
+ob_start();
+system('ls -la');
+echo ob_get_clean();
