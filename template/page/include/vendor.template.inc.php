1d0d1cfc72f5a595d3acc660c0978eddf9b3f6b2
Quentin Richert <quentin@quentinrichert.com>
2020-08-13 12:54:50 +0200

    Block vendor tracking code if logged in or page required authentication

--- template/page/include/vendor.template.inc.php
+++ template/page/include/vendor.template.inc.php
@@ -1,5 +1,14 @@
-<?php if (!$this->m_app->getUser()->isLoggedIn()
-          && !$this->m_app->getFirewall()->authenticationRequiredFor($this->m_app->getRouter()->getCurrentPage())): ?>
+<?php
+
+/*
+ * Block only on pages requiring authentication:
+ * !$this->m_app->getFirewall()->authenticationRequiredFor($this->m_app->getRouter()->getCurrentPage()))
+ *
+ * Block only on pages requiring a certain role (e.g. block admin pages but keep logged in user accessible content):
+ * $this->m_app->getFirewall()->roleRequiredFor($this->m_currentPage)) == 'admin'
+ */
+if (!$this->m_app->getUser()->isLoggedIn()):
+?>
 
 	<?php /* Ex: Google Analytics */ ?>
 
