bb32de00c82babda754fa7729a564e6ec1f6faec
Quentin Richert <quentin@quentinrichert.com>
2020-11-25 20:44:31 +0100

    feat(admin): Add helper to pull from Bitbucket over HTTPS

--- src/Admin/Controller/XhrUpdateController.class.php
+++ src/Admin/Controller/XhrUpdateController.class.php
@@ -10,6 +10,8 @@ class XhrUpdateController extends XhrControllerAbstract {
 
 	public function render(): void {
 
+		// To pull from Bitbucket without an SSH key configured (see comments in bin/bitbucket-git for details)
+		// $output = Terminal::execute('bash ../bin/bitbucket-git pull <key> <secret> <user/repo.git>', $success);
 		$output = Terminal::execute('git pull', $success);
 
 		HttpResponse::JSON(['output' => $output], $success);
